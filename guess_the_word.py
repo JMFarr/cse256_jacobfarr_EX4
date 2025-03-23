126129b19e652e7cbc44c7b5a814aad8a739faf5
Jacob Farr <17421798+JMFarr@users.noreply.github.com>
2025-03-23 16:39:40 -0700

    Remove check_guess and related tests.
    check_guess ended up being unneeded for the game's logic.

--- guess_the_word.py
+++ guess_the_word.py
@@ -2,14 +2,6 @@
 # CIS256 Spring 2025
 # EX 4
 
-def check_guess(remaining_letters, guess):
-    if len(remaining_letters) == 0:
-        return (False, "")
-    if remaining_letters[0] == guess:  
-        return (True, remaining_letters[1:])
-    else:
-        return (False, remaining_letters)
-
 def game():
     attempts_remaining = 10
     current_letter = 0
--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -2,14 +2,3 @@
 # CIS256 Spring 2025
 # EX 4
 
-from guess_the_word import check_guess
-
-class TestCheckGuessFunction:
-    def test_correct_guess_returns_expected_result(self):
-        assert check_guess("Letters", "L") == (True, "etters")
-    
-    def test_wrong_guess_returns_expected_result(self):
-        assert check_guess("Letters", "X") == (False, "Letters")
-
-    def test_guess_on_empty_string_returns_expected(self):
-        assert check_guess("", "L") == (False, "")
