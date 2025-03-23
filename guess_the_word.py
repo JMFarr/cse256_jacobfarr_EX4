c583874a979b56702666f87da6573f600c33135c
Jacob Farr <17421798+JMFarr@users.noreply.github.com>
2025-03-23 15:37:43 -0700

    Create function for checking a guess against remaining letters for word to guess.

--- guess_the_word.py
+++ guess_the_word.py
@@ -2,3 +2,11 @@
 # CIS256 Spring 2025
 # EX 4
 
+def check_guess(remaining_letters, guess):
+    if len(remaining_letters) == 0:
+        return (False, "")
+    if remaining_letters[0] == guess:  
+        return (True, remaining_letters[1:])
+    else:
+        return (False, remaining_letters)
+
