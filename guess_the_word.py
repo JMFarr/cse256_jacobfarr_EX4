2130d3b977321735466ad8849a86c051069ea8bb
Jacob Farr <17421798+JMFarr@users.noreply.github.com>
2025-03-23 16:37:42 -0700

    Add game function which contains main game loop.
    Game prompts user to enter a letter, checks it against the word to guess (set to "testing" for now, will be random in future), and displays a message based on if guess was correct or not.

--- guess_the_word.py
+++ guess_the_word.py
@@ -10,3 +10,24 @@ def check_guess(remaining_letters, guess):
     else:
         return (False, remaining_letters)
 
+def game():
+    attempts_remaining = 10
+    current_letter = 0
+    word_to_guess = "testing"
+    incorrect_guesses = set()
+    while (current_letter < len(word_to_guess)) and (attempts_remaining > 0):
+        guess = input("Guess a letter: ")
+        if guess.lower() == word_to_guess[current_letter].lower():
+            current_letter += 1
+            incorrect_guesses = set()
+            print("CORRECT!")
+        else:
+            attempts_remaining -= 1
+            incorrect_guesses.add(guess.lower())
+            print(f"WRONG. {attempts_remaining} attempts remaining.")
+        print(f"Word: {word_to_guess[:current_letter]}{"".join(["_" for _ in range(len(word_to_guess) - current_letter)])}")
+        print(f"Incorrect Guesses: {", ".join(incorrect_guesses)}")
+    if current_letter == len(word_to_guess):
+        print("Congratulations! You won!")
+    else:
+        print("You lost... Better luck next time.")
