7a1be4a660c38b4593928aafeba9acbfd498f1f7
Jacob Farr <17421798+JMFarr@users.noreply.github.com>
2025-03-23 17:06:32 -0700

    Add unit tests for check_guess function

--- test_guess_the_word.py
+++ test_guess_the_word.py
@@ -6,4 +6,9 @@ from guess_the_word import *
 
 def test_that_selected_word_is_from_list():
     assert select_random_word() in word_list
-    
+
+def test_check_guess_evaluates_correct_guesses_appropriately():
+    assert check_guess("l", "apple", 3) == True
+
+def test_check_guess_evaluates_wrong_guesses_appropriately():
+    assert check_guess("x", "apple", 0) == False
