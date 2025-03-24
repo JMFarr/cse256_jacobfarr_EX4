7c2a302fda3faef3c156568ba9e7b88966f7c2b1
Jacob Farr <17421798+JMFarr@users.noreply.github.com>
2025-03-23 17:03:11 -0700

    Refactor to introduce function for checking if guess is correct or not.
    Breaking the guess evaluation into a function so that it can be tested for correectness.

--- guess_the_word.py
+++ guess_the_word.py
@@ -28,6 +28,9 @@ word_list = ["hard",
 def select_random_word():
     return random.choice(word_list)
 
+def check_guess(guess, word, index):
+    return guess.lower() == word[index].lower()
+
 def game():
     attempts_remaining = 10
     current_letter = 0
@@ -35,7 +38,7 @@ def game():
     incorrect_guesses = set()
     while (current_letter < len(word_to_guess)) and (attempts_remaining > 0):
         guess = input("Guess a letter: ")
-        if guess.lower() == word_to_guess[current_letter].lower():
+        if check_guess(guess, word_to_guess, current_letter):
             current_letter += 1
             incorrect_guesses = set()
             print("CORRECT!")
